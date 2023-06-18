2fd8214703ce005e5b153f2c57c28b1f4cb6730e
sundaycrafts <sundaycrafts@users.noreply.github.com>
2023-06-18 02:30:16 +0200

    Make utilities importable from util/ instead of util namespace

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,4 +1,3 @@
 export { RequestCookies, ResponseCookies } from "./cookies.ts";
 export { EdgeSession } from "./edgesession.ts";
 export { SessionStore } from "./sessionStore.ts";
-export  * as util from "./util";
