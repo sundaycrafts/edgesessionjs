ffcfc529f8ad8d2808642fe0f8a61e652de056bd
sundaycrafts <sundaycrafts@users.noreply.github.com>
2023-06-21 22:23:00 +0200

    Exported missing module

--- packages/core/lib/index.ts
+++ packages/core/lib/index.ts
@@ -1,4 +1,5 @@
 export type {SessionState} from "./sessionState.ts";
 export type {RequestCookies, ResponseCookies} from "./cookies.ts";
 export {SessionStore} from "./sessionStore.ts";
+export {Signature} from "./signature.ts";
 export {EdgeSession} from "./edgesession.ts";
